df41a5ffe671cb7fc62e55001577b753c4bb3b99
Abhimanyu Kumar <abhimanyu392kumar@gmail.com>
2025-10-02 23:56:31 +0530

    spring batch initial commit

---
+++ src/test/java/com/av/spring/batch/springbatch/SpringBatchPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.av.spring.batch.springbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
